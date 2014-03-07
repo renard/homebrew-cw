91e5b860bcd8c75b7861d300b2f76fff54f262ef
Sébastien Gross <sebchezwamorg>
2014-03-07 01:07:50 +0100

    Add recipe for docopt.

---
+++ docopt.rb
@@ -0,0 +1,29 @@
+require 'formula'
+
+def which_python
+  "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+end
+
+class Docopt < Formula
+  homepage 'http://docopt.org'
+
+  git_url = 'https://github.com/docopt/docopt.git'
+
+  url git_url, :using => :git, :tag => '0.6.1'
+  head git_url, :using => :git, :branch => 'master'
+
+
+  depends_on :python
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+    
+    #python_lib = lib + which_python + 'site-packages'
+    #system "python", "setup.py", "build"
+    #python_lib.mkpath
+    system "python", "setup.py", "install", "--prefix=#{prefix}"
+  end
+
+  def test
+  end
+end
