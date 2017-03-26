7187f6f3210ead3aae1f02ba62262c7c01e4296b
Sébastien Gross <sebchezwamorg>
2017-03-26 15:27:29 +0200

    Update ricoh-driver-aficio-sp-c240dn
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- Casks/ricoh-driver-aficio-sp-c240dn.rb
+++ Casks/ricoh-driver-aficio-sp-c240dn.rb
@@ -1,9 +1,9 @@
 # Install via: brew cask ricoh-driver-aficio-sp-c240dn
-cask :v1 => 'ricoh-driver-aficio-sp-c240dn' do
-  version '2.022.02'
-  sha256 'ee97bb2775f62b69785996b475ed2b599872d618c4630f543a2603ef75f73c04'
+cask 'ricoh-driver-aficio-sp-c240dn' do
+  version '2.03'
+  sha256 '0dc9cd53ed6382b82b363969b15333096c7f25f8b99c930ed61ef524963b154f'
 
-  url 'http://support.ricoh.com/bb/pub_e/dr_ut_e/0001273/0001273748/V202/r69453Lf.dmg'
+  url 'http://support.ricoh.com/bb/pub_e/dr_ut_e/0001296/0001296979/V203/r76678Lf.dmg'
   name 'Ricoh Aficio SP C240DN driver'
   homepage 'http://support.ricoh.com/bb/html/dr_ut_e/ra/model/spc240/spc240fr.htm'
   license :closed 
